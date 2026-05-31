49c87497a00bf1f28d1d4d5afb6093cb9107e5dd
John Agbasiere <jchuks401@gmail.com>
2026-05-31 22:02:34 +0000

    Added calculator option

--- gitpractice/menu.sh
+++ gitpractice/menu.sh
@@ -1,32 +1,59 @@
-0#.
-!/bin/bash
+#!/bin/bash
+while true 
+do
+
 echo "Pick a number:"
-echo "1. say Hello"
-echo "2 CHeck age"
-echo "3 show date"
-echo "4. EXit "
-read number 
+echo "1. Say Hello"
+echo "2. Check Age"
+echo "3. Show Date"
+echo "4. Calculator"
+echo "5. Exit"
+
+read number
+
 if [ $number -eq 1 ]
-then 
-echo "What is your name?"
-read name
-echo "Hello $name"
+then
+    echo "What is your name?"
+    read name
+    echo "Hello $name"
+
 elif [ $number -eq 2 ]
-then 
-echo "How old are you? "
-read old 
-if [ $old -ge 18 ]
-then 
-echo "You are an adult"
-else 
-echo "YOu are under aged "
-fi
+then
+    echo "How old are you?"
+    read age
+
+    if [ $age -ge 18 ]
+    then
+        echo "You are an adult"
+    else
+        echo "You are underaged"
+    fi
+
 elif [ $number -eq 3 ]
-then 
-date
+then
+    date
+
 elif [ $number -eq 4 ]
 then
-echo "Goodbye"
+    echo " Enter first number: "
+read num1
+ 
+echo " Enter second number: "
+read num2 
+
+result=$((num1 + num2))
+echo " Result: $result"
+echo " Press enter to continue "
+read 
+
+elif [ $number -eq 5 ] 
+then 
+echo " Goodbye "
+break 
+
 else 
-echo "Invalid option  "
+    echo "Invalid option"
 fi
+done 
+
+
