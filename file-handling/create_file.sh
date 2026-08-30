8570b82693d059ad45db9b7dfec556ccd9793a03
John Agbasiere <jchuks401@gmail.com>
2026-08-30 21:09:17 +0000

    Added read file and updated create file

--- file-handling/create_file.sh
+++ file-handling/create_file.sh
@@ -1,8 +1,16 @@
-#!/bin/bash 
+#!/bin/bash
+ 
+ while true 
+  do 
 
-echo "Enter the name of the file you waant to create"
+  echo "Enter the name of the file you want to create"
 read filename 
-
-touch "$filename"
-
-echo "File '$filename' created successfully!"
+if [ -f "$filename" ]; 
+then 
+  echo "file alread exist!"
+else
+   touch "$filename"
+   echo "File '$filename' created successfully!"
+break
+fi
+done
